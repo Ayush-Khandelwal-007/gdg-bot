c078c7a020c9d42ad34c4a549fe63b7eb4484d1d
Ayush <ayushkhandelwal3217@gmail.com>
2021-10-02 15:45:42 +0530

    logs
    Signed-off-by: Ayush <ayushkhandelwal3217@gmail.com>

--- web.js
+++ web.js
@@ -6,9 +6,11 @@ app.use(bodyParser.json());
 
 app.post('/', function (req, res) {
   console.log("Response",req.body.arena);
-  const bots=Object.entries(req.body.arena.state);
+  const bots=Object.entries(req.body.arena.state).filter(bot=>bot[0]!=="https://nodejs-bot-6evexe43kq-em.a.run.app");
 
-  console.log('players = ',bots)
+
+
+  console.log('players = ',Object.entries(req.body.arena.state).length," - 1 = " ,bots.length);
 
   const moves = ['F', 'T', 'L', 'R'];
   res.send('T');
